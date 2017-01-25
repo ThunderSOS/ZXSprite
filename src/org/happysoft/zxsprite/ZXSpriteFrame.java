955a9504e7129d7ea4604ed2e8bb62d7efd79263
ThunderSOS <c_francis1@yahoo.com>
2017-01-25 14:44:57 +0000

    New sprite attributes driven off an option pane from the 'New' menu option.

--- src/org/happysoft/zxsprite/ZXSpriteFrame.java
+++ src/org/happysoft/zxsprite/ZXSpriteFrame.java
@@ -3,64 +3,68 @@
  */
 package org.happysoft.zxsprite;
 
+import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
+import javax.swing.JComboBox;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
 
 /**
  * @author Chris Francis (c_francis1@yahoo.com)
  */
 public class ZXSpriteFrame extends JFrame {
 
-  JMenu fileMenu = new JMenu("File");
-  JMenuItem animation_8_8 = new JMenuItem("New Sprite 8x8");
-  JMenuItem animation_8_16 = new JMenuItem("New Sprite 8x16");
-  JMenuItem animation_16_16 = new JMenuItem("New Sprite 16x16");
-  JMenuItem animation_8_8_p = new JMenuItem("New Sprite 8x8 - Pre-shifted(16x8)");
-  JMenuItem animation_8_16_p = new JMenuItem("New Sprite 8x16 - Pre-shifted(16x16)");
-  JMenuItem animation_16_16_p = new JMenuItem("New Sprite 16x16 - Pre-shifted(24x16)");
-  
-  JMenuItem open = new JMenuItem("Open");
-  JMenuItem save = new JMenuItem("Save");
+  private JMenu fileMenu = new JMenu("File");
 
-  JMenu editMenu = new JMenu("Edit");
-  
-  JMenuItem copy = new JMenuItem("Copy");
-  JMenuItem paste = new JMenuItem("Paste");
-  JMenuItem scrollLeft = new JMenuItem("Shift Left");
-  JMenuItem scrollRight = new JMenuItem("Shift Right");
-  JMenuItem reset = new JMenuItem("Reset");
-  
-  JScrollPane scrollPane;
-  
-  AnimationFrameTabPanel animationFrameTabPanel;
-  
+  private JMenuItem open = new JMenuItem("Open");
+  private JMenuItem save = new JMenuItem("Save");
+
+  private JMenu editMenu = new JMenu("Edit");
+
+  private JMenuItem copy = new JMenuItem("Copy");
+  private JMenuItem paste = new JMenuItem("Paste");
+  private JMenuItem scrollLeft = new JMenuItem("Shift Left");
+  private JMenuItem scrollRight = new JMenuItem("Shift Right");
+  private JMenuItem reset = new JMenuItem("Reset");
+
+  private JScrollPane scrollPane;
+
+  private AnimationFrameTabPanel animationFrameTabPanel;
+
+  private JComboBox<Integer> width = new JComboBox(new Integer[]{8, 16, 24, 32});
+  private JComboBox<Integer> height = new JComboBox(new Integer[]{8, 16, 24, 32});
+  private JComboBox<Integer> frames = new JComboBox(new Integer[]{1, 2, 4, 8});
+
   private ZeusDgDefaultFormat zeusDgFormat = new ZeusDgDefaultFormat();
-  
+
   private boolean[][] buffer = null;
 
   public ZXSpriteFrame(String title) {
-    super(title);    
+    super(title);
     jinit();
   }
 
   private void jinit() {
+    frames.setSelectedItem(4);
+    JPanel spriteOptionPanel = createOptionPanel();
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     JMenuBar menuBar = new JMenuBar();
+    JMenuItem newMenu = new JMenuItem("New");
 
-    JMenu newMenu = new JMenu("New");
-    newMenu.add(animation_8_8);
-    newMenu.add(animation_8_16);
-    newMenu.add(animation_16_16);
-    
     fileMenu.add(newMenu);
-    
+
     fileMenu.add(open);
     fileMenu.add(save);
 
@@ -69,73 +73,86 @@ public class ZXSpriteFrame extends JFrame {
     editMenu.add(scrollLeft);
     editMenu.add(scrollRight);
     editMenu.add(reset);
-    
-    animation_8_8.addActionListener((ActionEvent e) -> {
-      addAnimationTabPanel(8, 8, 4);
+
+    newMenu.addActionListener((ActionEvent e) -> {
+      int option = JOptionPane.showOptionDialog(null, spriteOptionPanel, "Select sprite attributes",
+              JOptionPane.OK_CANCEL_OPTION,
+              JOptionPane.QUESTION_MESSAGE, null, null, null);
+      if (option == JOptionPane.CANCEL_OPTION) {
+        // user hit cancel
+      } else if (option == JOptionPane.OK_OPTION) {
+        Integer w = (Integer) width.getSelectedItem();
+        Integer h = (Integer) height.getSelectedItem();
+        Integer f = (Integer) frames.getSelectedItem();
+        addAnimationTabPanel(w, h, f);
+      }
     });
-    
-    animation_8_16.addActionListener((ActionEvent e) -> {
-      addAnimationTabPanel(8, 16, 4);
-    });
-    
-    animation_16_16.addActionListener((ActionEvent e) -> {
-      addAnimationTabPanel(16, 16, 4);
-    });   
-        
+
     reset.addActionListener((ActionEvent e) -> {
       animationFrameTabPanel.getSelectedSpriteToggler().clear();
     });
-    
+
     copy.addActionListener((ActionEvent e) -> {
       buffer = animationFrameTabPanel.getSelectedSpriteToggler().getFilledSquares();
     });
-   
+
     paste.addActionListener((ActionEvent e) -> {
-      if(buffer != null) {
+      if (buffer != null) {
         animationFrameTabPanel.getSelectedSpriteToggler().setFilledSquares(buffer);
       }
       buffer = null;
     });
-    
+
     scrollRight.addActionListener((ActionEvent e) -> {
-      animationFrameTabPanel.getSelectedSpriteToggler().shiftRight();    
+      animationFrameTabPanel.getSelectedSpriteToggler().shiftRight();
       buffer = null;
     });
-    
+
     scrollLeft.addActionListener((ActionEvent e) -> {
-      animationFrameTabPanel.getSelectedSpriteToggler().shiftLeft();    
+      animationFrameTabPanel.getSelectedSpriteToggler().shiftLeft();
       buffer = null;
     });
-    
+
     save.addActionListener((ActionEvent e) -> {
       SpriteToggler st = animationFrameTabPanel.getSelectedSpriteToggler();
       zeusDgFormat.export(st.getGridWidth(), st.getGridHeight(), st.getFilledSquares());
     });
-    
+
     setPreferredSize(new Dimension(760, 500));
-       
+
     menuBar.add(fileMenu);
     menuBar.add(editMenu);
 
     setJMenuBar(menuBar);
-    
+
     scrollPane = new JScrollPane();
     add(scrollPane);
   }
-  
+
+  private JPanel createOptionPanel() {
+    JPanel p = new JPanel(new FlowLayout());
+    JLabel widthLabel = new JLabel("Width");
+    JLabel heightLabel = new JLabel("Height");
+    JLabel framesLabel = new JLabel("Num Frames");
+    p.add(widthLabel);
+    p.add(width);
+    p.add(heightLabel);
+    p.add(height);
+    p.add(framesLabel);
+    p.add(frames);
+    return p;
+  }
+
   public void addAnimationTabPanel(int width, int height, int frames) {
     animationFrameTabPanel = new AnimationFrameTabPanel(this, width, height, frames);
-    
     Dimension d = animationFrameTabPanel.getPreferredSize();
     scrollPane.setViewportView(animationFrameTabPanel);
     setSize(d);
     repaint();
   }
-  
+
   public void enableMenus(boolean enabled) {
     copy.setEnabled(enabled);
-    scrollLeft.setEnabled(enabled);
-    scrollRight.setEnabled(enabled);
   }
 
 }
